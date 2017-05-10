1b82e6aee98e3aa095016481679d2af6deb4d142
iaptx4869 <j.1x@qq.com>
2017-05-11 02:29:49 +0800

    Create testSuper

---
+++ testSuper.java
@@ -0,0 +1,18 @@
+
+class SuperClass {
+    int i = 50;
+}
+
+class SubClass extends SuperClass {
+    int i = 100;
+
+    public void showMessage() {
+        System.out.printf("super.i = %d, this.i = %d\n", super.i, this.i);
+    }
+}
+
+class testSuper {
+    public static void main(String[] args) {
+        new SubClass().showMessage();
+    }
+}
